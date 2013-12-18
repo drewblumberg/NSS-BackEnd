c3ed8b03ae08455427a94f1c93193d09b863fabf
Drew Blumberg <alblumberg21@gmail.com>
2013-12-18 00:24:25 -0600

    Passed 2 original tests and added test for names with vowels

--- cheers/test/test_cheers_integration.rb
+++ cheers/test/test_cheers_integration.rb
@@ -11,9 +11,10 @@ class TestCheersIntegration < MiniTest::Unit::TestCase
     end
     expected_output = <<EOS
 What's your name?
-Give me a.. B
-Give me a.. R
-Give me a.. T
+Give me a... B
+Give me an... R
+Give me a... T
+You're the best, Brt!
 EOS
     assert_equal shell_output, expected_output
   end
@@ -25,8 +26,25 @@ EOS
       pipe.close_write
       shell_output = pipe.read
     end
-    assert_includes_in_order shell_output, "Give me a.. B", "Give me a.. R", "Give me a.. T"
+    assert_includes_in_order shell_output, "Give me a... B", "Give me an... R", "Give me a... T"
   end
 
   # But what about names with vowels?!!
+  def test_a_name_with_vowels
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts("Drew")
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = <<EOS
+What's your name?
+Give me a... D
+Give me an... R
+Give me an... E
+Give me a... W
+You're the best, Drew!
+EOS
+    assert_equal shell_output, expected_output
+  end
 end
