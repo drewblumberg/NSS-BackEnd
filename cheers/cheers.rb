0f821f8107cfa0c5c327ed888927c9cb53b36daa
Drew Blumberg <alblumberg21@gmail.com>
2013-12-17 23:57:52 -0600

    Initial NSS Backend commit

---
+++ cheers/cheers.rb
@@ -0,0 +1,10 @@
+puts "What's your name?"
+name = gets.chomp
+name_array = name.downcase.split('')
+an_array = ['a', 'e', 'f', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 'x']
+name_array.each do |letter|
+  # an_array.include?(letter) ? diff = 'an' : diff = 'a'
+  diff = an_array.include?(letter) ? 'an' : 'a'
+  puts "Give me " + diff + "... " + letter.capitalize
+end
+puts "You're the best, " + name.capitalize + '!'
