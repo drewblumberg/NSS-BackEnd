7f0b3e46124c473b1e877a5db275be37b2fa4bb8
Drew Blumberg <alblumberg21@gmail.com>
2013-12-19 01:14:17 -0600

    5 basic tests for DateParser

--- unit-testing-exercise/date_parser.rb
+++ unit-testing-exercise/date_parser.rb
@@ -1,5 +1,18 @@
 class DateParser
   def self.parse(date_string)
+    date_symbol_index = date_string =~ /[\/.-]/
+    date_symbol = date_string[date_symbol_index] if date_symbol_index
+    if !date_symbol.nil?
+      sub_dates = date_string.split(date_symbol)
+      if Integer(sub_dates[0]) > 12
+        date_string = "#{sub_dates[1]}/#{sub_dates[0]}/#{sub_dates[2]}"
+      else
+        date_string = "#{sub_dates[0]}/#{sub_dates[1]}/#{sub_dates[2]}"
+      end
+    else
+      date_string = "01/01/1900"
+    end
 
+    date_string
   end
 end
--- unit-testing-exercise/test_date_parser.rb
+++ unit-testing-exercise/test_date_parser.rb
@@ -5,4 +5,29 @@ class TestDateParser < MiniTest::Unit::TestCase
   def test_date_parser_has_a_parse_method
     DateParser.parse("2004/12/24")
   end
+
+  def test_date_parser_US_date
+    date = DateParser.parse("12/15/1986")
+    assert_equal date, "12/15/1986"
+  end
+
+  def test_date_parser_EU_date
+    date = DateParser.parse("15/12/1986")
+    assert_equal date, "12/15/1986"
+  end
+
+  def test_date_parser_no_input
+    date = DateParser.parse("")
+    assert_equal date, "01/01/1900"
+  end
+
+  def test_date_parser_dots_date
+    date = DateParser.parse("12.15.1986")
+    assert_equal date, "12/15/1986"
+  end
+
+  def test_date_parser_dashes_date
+    date = DateParser.parse("12-15-1986")
+    assert_equal date, "12/15/1986"
+  end
 end
